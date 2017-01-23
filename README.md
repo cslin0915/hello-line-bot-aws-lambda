95f4898fc4d26319936a0b6235d6e9e79f13f682
Chih-Sheng <note0972@gmail.com>
2017-01-23 15:09:01 +0800

    2017-01-23 15:08

--- README.md
+++ README.md
@@ -1,15 +1,26 @@
-# Creating a LINE BOT on AWS Lambda
+Creating a LINE BOT on AWS Lambda
+=================================
 
-## References 
+References
+----------
 
 ### AWS Lambda
 
-- [Java Logging](http://docs.aws.amazon.com/lambda/latest/dg/java-logging.html)
+-	[Lambda Function Handler (Java)](http://docs.aws.amazon.com/lambda/latest/dg/java-programming-model-handler-types.html)
+-	[Creating A Simple REST Service Using AWS Lambda, API Gateway, And IAM](http://technology.finra.org/code/enjoying-auto-scaling-integrated-authentication-low-host-cost.html)
+-	[Java Logging](http://docs.aws.amazon.com/lambda/latest/dg/java-logging.html)
+
+### AWS Gateway
+
+-	[Use Postman to Test an API ](http://docs.aws.amazon.com/apigateway/latest/developerguide/how-to-use-postman-to-call-api.html)
 
 ### LINE BOT and Lambda
 
-- [LINE Messaging API と AWS Lambda で LINE BOT を作ってみた](http://www.kazuweb.asia/aws/lambda/chatbot)
+-	[LINE Messaging API と AWS Lambda で LINE BOT を作ってみた](http://www.kazuweb.asia/aws/lambda/chatbot)
 
 ### Spring Boot and Lambda (@Deprecated)
 
-- [Spring BootをAWS Lambdaで動かす](http://dev.classmethod.jp/server-side/java/spring-boot-aws-lambda-handler-appliation-context/)
+-	[Creating AWS Lambda using java and spring framework](https://apiexplorer.wordpress.com/2016/03/11/creating-aws-lambda-using-java-and-spring-framework/)
+-	[Making Spring Boot application run serverless with AWS](https://www.javacodegeeks.com/2016/12/making-spring-boot-application-run-serverless-aws.html)
+-	[Spring BootをAWS Lambdaで動かす](http://dev.classmethod.jp/server-side/java/spring-boot-aws-lambda-handler-appliation-context/)
+-	[API Gateway + AWS Lambda + Spring Boot でCRUD APIを作成してみた](http://dev.classmethod.jp/server-side/java/api-gateway-aws-lambda-spring-boot-crud-api/)
--- src/main/java/jasonlin/helloworld/HelloLineBot.java
+++ src/main/java/jasonlin/helloworld/HelloLineBot.java
@@ -26,6 +26,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.UncheckedIOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -36,34 +37,30 @@ public class HelloLineBot implements RequestHandler<InputStream, Object> {
     private static LineMessagingService lineMessagingService;
 
     @Override
-    public Object handleRequest(InputStream input, Context context) {
+    public String handleRequest(InputStream input, Context context) {
         logger = context.getLogger();
-        String json = null;
         try {
-            createLintBotLog4Appender();
-            lineMessagingService = LineMessagingServiceBuilder.create(System.getenv("LINE_BOT_CHANNEL_TOKEN")).build();
-            json = IOUtils.toString(input, "UTF-8");
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-            objectMapper.registerModule(new JavaTimeModule())
-                        .configure(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS, false);
-            CallbackRequest callbackRequest = objectMapper.readValue(json, CallbackRequest.class);
-            Event e = callbackRequest.getEvents().get(0);
+            buildLineMessagingService();
+            Event e = getLineCallbackEvent(input);
             if (e instanceof MessageEvent){
                 MessageEvent event = (MessageEvent) e;
                 MessageContent messageContent = event.getMessage();
                 if (messageContent instanceof TextMessageContent) {
                     MessageEvent<TextMessageContent> messageEvent = (MessageEvent<TextMessageContent>) e;
                     handleTextMessageEvent(messageEvent);
+                } else {
+                    String replyToken = event.getReplyToken();
+                    String replyMessage = "Sorry, I can only reply text messages now.";
+                    this.reply(replyToken, new TextMessage(replyMessage));
                 }
             } else {
-                logger.log("It isn't a MessageEvent instance... : " + json + "\n");
+                logger.log("It isn't a MessageEvent instance...\n");
             }
         } catch (Exception e) {
             logger.log("It isn't a Line Message...\n");
             e.printStackTrace();
         }
-        return "success";
+        return "Success to call the Lambda function.";
     }
 
     private void handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws IOException{
@@ -74,7 +71,7 @@ public class HelloLineBot implements RequestHandler<InputStream, Object> {
     private void handleTextContent(String replyToken, Event event, TextMessageContent content) throws IOException {
         String text = content.getText();
         String helpText = "(I am an echo bot now!)";
-        logger.log(String.format("Returns echo message %s: %s", replyToken, text) + "\n");
+        logger.log(String.format("Returns echo message : %s", text) + "\n");
 
         this.reply(
                 replyToken,
@@ -83,6 +80,10 @@ public class HelloLineBot implements RequestHandler<InputStream, Object> {
                         new TextMessage(helpText)
                 )
         );
+    }
+
+    private void reply(@NonNull String replyToken, @NonNull Message message) throws IOException {
+        reply(replyToken, Collections.singletonList(message));
     }
 
     private void reply(@NonNull String replyToken, @NonNull List<Message> messages) throws IOException {
@@ -96,11 +97,18 @@ public class HelloLineBot implements RequestHandler<InputStream, Object> {
         }
     }
 
-    private void createLintBotLog4Appender() throws Exception {
-        String targetLog="com.linecorp.bot";
-        ConsoleAppender apndr = new ConsoleAppender(new PatternLayout("%d{yyyy-MM-dd HH:mm:ss} %-5p %c{1}:%L - %m%n"));
-        Logger log4jLogger = Logger.getLogger(targetLog);
-        log4jLogger.addAppender(apndr);
-        log4jLogger.setLevel((Level) Level.ALL);
+    private void buildLineMessagingService() {
+        lineMessagingService = LineMessagingServiceBuilder.create(System.getenv("LINE_BOT_CHANNEL_TOKEN")).build();
+    }
+
+    private Event getLineCallbackEvent(InputStream lambdaInput) throws Exception {
+        String json = IOUtils.toString(lambdaInput, "UTF-8");
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.registerModule(new JavaTimeModule())
+                    .configure(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS, false);
+        CallbackRequest callbackRequest = objectMapper.readValue(json, CallbackRequest.class);
+        Event e = callbackRequest.getEvents().get(0);
+        return e;
     }
 }
